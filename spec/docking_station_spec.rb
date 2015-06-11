a053cad31b9e8f8b96e0cfd6e880a827df5d5d68
Christian Miller <cwgmiller@gmail.com>
2015-06-11 15:32:48 +0100

    Third User Story Complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,9 +9,16 @@ describe DockingStation do
 	end
 	it { is_expected.to respond_to(:dock).with(1).argument }
 
-	describe 'realease_bike' do
+	describe 'release_bike' do
 		it 'raises an error when there are no bikes available' do 
 			expect {subject.release_bike}.to raise_error 'No bikes available'
+		end
+	end
+
+	describe 'dock' do 
+		it 'raises an error when at capacity' do 
+			subject.dock Bike.new
+			expect {subject.dock Bike.new}.to raise_error 'Docking station full'
 		end
 	end
 end
